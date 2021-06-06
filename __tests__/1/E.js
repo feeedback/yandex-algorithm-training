ddd79d5ea9cb6d2f6282daa0c1ad8bf4c7dd79f4
A. Rzhanitsyn <risdeep@gmail.com>
2021-06-06 16:19:30 +0500

    fix test

--- __tests__/1/E.js
+++ __tests__/1/E.js
@@ -1,10 +1,6 @@
 import { test, expect } from '@jest/globals';
 import fn from '../../lessons/1/E.js';
 
-// test('deep diff JSON files. format "stylish" (default)', () => {
-//   expect(getDiff(filepathJsonA, filepathJsonB)).toBe(expectedStylish);
-// });
-
 test('1', () => {
   expect(fn(['89 20 41 1 11'])).toStrictEqual('2 3');
 });
