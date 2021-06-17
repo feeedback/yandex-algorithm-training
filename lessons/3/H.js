5e96ee3bfd5402088284066246e33d494f49e163
A. Rzhanitsyn <risdeep@gmail.com>
2021-06-17 23:26:38 +0500

    3 H - done

--- lessons/3/H.js
+++ lessons/3/H.js
@@ -20,97 +20,17 @@
 // одно целое число — минимальное количество выстрелов, необходимое для того, чтобы сбить всех птиц.
 
 function inputProcessing(lines) {
-  const [NRaw, ...birdsXYRaw] = lines;
-  const N = Number(NRaw); // N (1 ≤ N ≤ 1000)
+  const [, ...birdsXYRaw] = lines;
+  // const N = Number(NRaw); // N (1 ≤ N ≤ 1000)
 
   const birdsXY = birdsXYRaw.map((xy) => xy.split(' ').map(Number));
 
-  const mapXToBirdsXY = {};
-  for (const [x, y] of birdsXY) {
-    if (!mapXToBirdsXY[x]) {
-      mapXToBirdsXY[x] = [];
-    }
-    mapXToBirdsXY[x].push([x, y]);
-  }
-
-  console.log(mapXToBirdsXY);
-  // const [pigX, pigY] = [0, 0];
-  const maxXY = Math.max(...birdsXY.flat());
-  console.log({ N });
-  console.log({ birdsXY, maxXY });
-
-  const mapDefinitionToSymbol = { MINE: '*', ZERO_MINES_NEARBY: 0 };
-
-  class Minesweeper {
-    constructor(x = 9, y = 9, mines = 10, minesXY = 0) {
-      if (mines > x * y) {
-        throw new Error('ERROR: mines more than cell');
-      }
-      this.x = x;
-      this.y = y;
-      this.mines = mines;
-      this.birdsXY = minesXY;
-
-      this.init();
-    }
-
-    _initFieldAndFillValue(value) {
-      return Array.from({ length: this.y }, () => new Array(this.x).fill(value));
-    }
-
-    _getArea8(cellX, cellY) {
-      const area8 = [];
-      for (let y = cellY - 1; y <= cellY + 1; y += 1) {
-        for (let x = cellX - 1; x <= cellX + 1; x += 1) {
-          if (y >= 0 && y < this.y && x >= 0 && x < this.x && !(cellX === x && cellY === y)) {
-            area8.push({ x, y });
-          }
-        }
-      }
-      return area8;
-    }
-
-    _increaseMineCounterInArea8(mineX, mineY) {
-      this._getArea8(mineX, mineY).forEach(({ x, y }) => {
-        const cellValue = this.field[y][x];
-        if (cellValue !== mapDefinitionToSymbol.MINE) {
-          this.field[y][x] = cellValue + 1;
-        }
-      });
-    }
-
-    _calculateMinesAndSetCounterValues() {
-      this.field.forEach((row, y) =>
-        row.forEach((cell, x) => {
-          if (cell === mapDefinitionToSymbol.MINE) {
-            this._increaseMineCounterInArea8(x, y);
-          }
-        })
-      );
-    }
-
-    _initMines() {
-      for (const [x, y] of this.birdsXY) {
-        this.field[y][x] = mapDefinitionToSymbol.MINE;
-      }
-    }
-
-    init() {
-      this.field = this._initFieldAndFillValue(mapDefinitionToSymbol.ZERO_MINES_NEARBY);
-      this._initMines();
-      // this._calculateMinesAndSetCounterValues();
-    }
-  }
-
-  const game = new Minesweeper(maxXY + 1, maxXY + 1, N, birdsXY);
-
-  game.field.forEach((y) => console.log(y.join(' ')));
-  return Object.keys(mapXToBirdsXY).length;
+  return new Set(birdsXY.map(([x]) => x)).size;
 }
 
 (async () => {
-  // const inputLines = ['6', '1 1', '2 2', '3 3', '2 1', '3 2', '3 1'];
-  const inputLines = [6, '1 1', '2 2', '3 3', '2 1', '3 2', '3 4'];
+  const inputLines = ['6', '1 1', '2 2', '3 3', '2 1', '3 2', '3 1'];
+  // const inputLines = [6, '1 1', '2 2', '3 3', '2 1', '3 2', '3 4'];
   console.log({ inputLines });
 
   const outputLines = inputProcessing(inputLines);
