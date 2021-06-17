a80cb2fd859641e7d09928718d72ae303d2627df
A. Rzhanitsyn <risdeep@gmail.com>
2021-06-17 23:33:05 +0500

    rewrap comments

--- lessons/3/B.js
+++ lessons/3/B.js
@@ -1,6 +1,8 @@
 // https://contest.yandex.ru/contest/27663/problems/B/
 
-// Даны два списка чисел, которые могут содержать до 10000 чисел каждый. Выведите все числа, которые входят как в первый, так и во второй список в порядке возрастания. Примечание. И даже эту задачу на Питоне можно решить в одну строчку.
+// Даны два списка чисел, которые могут содержать до 10000 чисел каждый. Выведите все числа,
+// которые входят как в первый, так и во второй список в порядке возрастания. Примечание. И даже
+// эту задачу на Питоне можно решить в одну строчку.
 
 // Формат ввода
 // Вводятся два списка целых чисел. Все числа каждого списка находятся на отдельной строке.
@@ -12,10 +14,10 @@ function inputProcessing(lines) {
   const set1 = new Set(lines[0].split(' ').map(Number));
   const set2 = new Set(lines[1].split(' ').map(Number));
 
-  const intersection = []
+  const intersection = [];
   for (const num of set1) {
     if (set2.has(num)) {
-      intersection.push(num)
+      intersection.push(num);
     }
   }
   return intersection.sort((a, b) => a - b);
@@ -23,10 +25,7 @@ function inputProcessing(lines) {
 
 (async () => {
   // const inputLines = await input(1);
-  const inputLines = ['1 3 2',
-    '4 3 2'
-  ];
-
+  const inputLines = ['1 3 2', '4 3 2'];
 
   // console.log({ inputLines });
   const outputLines = inputProcessing(inputLines);
