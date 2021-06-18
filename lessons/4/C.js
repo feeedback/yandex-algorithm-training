44adc789f08e0ab96091ae5c0c5d7a8cff33cb25
A. Rzhanitsyn <risdeep@gmail.com>
2021-06-18 16:44:32 +0500

    4 C - done

--- lessons/4/C.js
+++ lessons/4/C.js
@@ -1,4 +1,4 @@
-// https://contest.yandex.ru/contest/27665/problems/B
+// https://contest.yandex.ru/contest/27665/problems/C
 
 // Во входном файле (вы можете читать данные из файла input.txt) записан текст. Словом считается
 // последовательность непробельных символов идущих подряд, слова разделены одним или большим числом
@@ -18,22 +18,30 @@ function inputProcessing() {
   const data = fs.readFileSync('input.txt', 'utf8');
   const words = data.match(/[^\s\n]+/g);
   const mapWordToCount = {};
-  const mapCountEarlier = [];
 
   if (!words) {
     return '';
   }
   for (const word of words) {
     if (!mapWordToCount[word]) {
-      mapWordToCount[word] = 1;
-      mapCountEarlier.push(0);
-    } else {
-      mapCountEarlier.push(mapWordToCount[word]);
-      mapWordToCount[word] += 1;
+      mapWordToCount[word] = 0;
     }
+    mapWordToCount[word] += 1;
   }
-  // console.log({ words, mapWordToCount, mapCountEarlier });
-  return mapCountEarlier.join(' ');
+  const mapCountToWord = {};
+
+  for (const [word, count] of Object.entries(mapWordToCount)) {
+    if (!mapCountToWord[count]) {
+      mapCountToWord[count] = [];
+    }
+    mapCountToWord[count].push(word);
+  }
+  const [, wordsMaxCount] = Object.entries(mapCountToWord).sort(([countA], [countB]) => +countB - +countA)[0];
+  const wordsFreq = wordsMaxCount.sort((countA, countB) => String(countA).localeCompare(countB));
+  const [wordMaxFreq] = wordsFreq;
+
+  // console.log({ words, mapWordToCount, mapCountToWord, wordMaxFreq });
+  return wordMaxFreq;
 }
 
 process.stdout.write(String(inputProcessing()));
