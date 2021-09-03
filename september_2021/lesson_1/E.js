28435651f06d51aa44f1fd8bd110c6d7b5ec1982
A. Rzhanitsyn <risdeep@gmail.com>
2021-09-03 16:08:06 +0500

    add september_2021\lesson_1\E

--- september_2021/lesson_1/E.js
+++ september_2021/lesson_1/E.js
@@ -1,19 +1,12 @@
-const getAreaTriangleByCoords = ({ A: [xA, yA], B: [xB, yB], C: [xC, yC] }) =>
-  Math.abs((xB - xA) * (yC - yA) - (xC - xA) * (yB - yA)) / 2;
+const getTrianglePointVectorProducts = ({ A: [xA, yA], B: [xB, yB], C: [xC, yC] }, [pointX, pointY]) => {
+  const ABxAP = (xA - pointX) * (yB - yA) - (xB - xA) * (yA - pointY);
+  const BCxBP = (xB - pointX) * (yC - yB) - (xC - xB) * (yB - pointY);
+  const CAxCP = (xC - pointX) * (yA - yC) - (xA - xC) * (yC - pointY);
+
+  return [ABxAP, BCxBP, CAxCP];
+};
 
 const distanceBetweenTwoPoint = ([x1, y1], [x2, y2]) => Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-
-// const getSidesTriangle = ({ A: [xA, yA], B: [xB, yB], C: [xC, yC] }) => {
-//   const a = Math.sqrt((xC - xB) ** 2 + (yC - yB) ** 2); // BC
-//   const b = Math.sqrt((xC - xA) ** 2 + (yC - yA) ** 2); // AC
-//   const c = Math.sqrt((xB - xA) ** 2 + (yB - yA) ** 2); // AB
-//   return { a, b, c };
-// };
-// const getAreaTriangle = ({ a, b, c }) => {
-//   const p = (a + b + c) / 2;
-//   const area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-//   return area;
-// };
 
 /**
  * "E. Точка и треугольник" {@link "https://contest.yandex.ru/contest/28730/problems/E}
@@ -28,6 +21,42 @@ const distanceBetweenTwoPoint = ([x1, y1], [x2, y2]) => Math.sqrt((x2 - x1) ** 2
  *  меньше.
  */
 function calcBetterCoordForBuildSchool(cathetus, pointXY) {
+  const triangleVertices = { A: [0, 0], B: [cathetus, 0], C: [0, cathetus] };
+  const vectorProducts = getTrianglePointVectorProducts(triangleVertices, pointXY);
+
+  if (
+    vectorProducts.some((v) => v === 0) ||
+    vectorProducts.every((v) => v > 0) ||
+    vectorProducts.every((v) => v < 0)
+  ) {
+    // Если все три значения одинакового знака, то точка внутри треугольника.
+    // Если значение равно нулю, значит точка лежит на стороне треугольника.
+    // В ином случае, точка вне треугольника.
+    return 0;
+  }
+
+  const distanceFromPointToTriangleVertices = Object.values(triangleVertices).map((coord) =>
+    distanceBetweenTwoPoint(pointXY, coord)
+  );
+  const minDistanceToVerticesTriangleFromPoint = Math.min(...distanceFromPointToTriangleVertices);
+
+  return distanceFromPointToTriangleVertices.indexOf(minDistanceToVerticesTriangleFromPoint) + 1;
+}
+
+// eslint-disable-next-line no-unused-vars
+function inputProcessing(lines) {
+  const cathetus = Number(lines[0]);
+  const pointXY = lines[1].split(' ').map(Number);
+
+  return calcBetterCoordForBuildSchool(cathetus, pointXY);
+}
+
+// SECOND
+const getAreaTriangleByCoords = ({ A: [xA, yA], B: [xB, yB], C: [xC, yC] }) =>
+  Math.abs((xB - xA) * (yC - yA) - (xC - xA) * (yB - yA)) / 2;
+
+// eslint-disable-next-line no-unused-vars
+function calcBetterCoordForBuildSchoolByArea(cathetus, pointXY) {
   // Метод сравнения площадей - по формуле Герона находятся площади 3-х треугольников которые
   // образует точка с каждой стороной треугольника, далее находится площадь самого треугольника и
   // сравнивается с суммой 3ех предыдущих треугольников, если суммы равны то значит точка
@@ -55,13 +84,5 @@ function calcBetterCoordForBuildSchool(cathetus, pointXY) {
   const minDistanceToVerticesTriangleFromPoint = Math.min(...distanceFromPointToTriangleVertices);
 
   return distanceFromPointToTriangleVertices.indexOf(minDistanceToVerticesTriangleFromPoint) + 1;
-}
-
-// eslint-disable-next-line no-unused-vars
-function inputProcessing(lines) {
-  const cathetus = Number(lines[0]);
-  const pointXY = lines[1].split(' ').map(Number);
-
-  return calcBetterCoordForBuildSchool(cathetus, pointXY);
 }
 export default inputProcessing;
