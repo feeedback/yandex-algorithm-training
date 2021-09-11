7673de7d6d1dadd1cc3128e7863151705e528587
A. Rzhanitsyn <risdeep@gmail.com>
2021-09-11 18:03:53 +0500

    add september_2021\lesson_4\A полное решение

--- september_2021/lesson_4/A.js
+++ september_2021/lesson_4/A.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-nested-ternary */
 /**
  * "A. Толя-Карп и новый набор структур, часть 2" {@link "https://contest.yandex.ru/contest/28970/problems/A/}
  *
  * Чему будут равны значения чисел, если сложить между собой все те, что имеют одинаковый цвет.
  * Напишите, пожалуйста, программу, которая выводит результат.
  *
- * @param { number } count  число n (0 ≤ n ≤ 2*10^5)
- * @param { Array<[number, number] } colorWithValues список по два числа: цвет числа в ящике di и
+ * @param { BigInt } count  число n (0 ≤ n ≤ 2*10^5)
+ * @param { Array<[BigInt, BigInt] } colorWithValues список по два числа: цвет числа в ящике di и
  * значение числа ai (-10^18 ≤ di ai ≤ 10^18).
  *
  * @return { string[] } Выведите в порядке возрастания номера цвета пары чисел, каждая в новой
@@ -16,16 +17,17 @@ function calcColorsBoxCounts(count, colorWithValues) {
 
   for (let index = 0; index < count; index++) {
     const [color, value] = colorWithValues[index];
-
-    colorsSums.set(color, (colorsSums.get(color) || 0) + value);
+    colorsSums.set(color, BigInt(colorsSums.get(color) || 0) + value);
   }
 
-  return [...colorsSums].sort(([a], [b]) => a - b).map((p) => p.join(' '));
+  const colors = [...colorsSums.keys()].sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+
+  return colors.map((color) => `${color} ${colorsSums.get(color)}`);
 }
 
 function inputProcessing(lines) {
-  const count = Number(lines[0]);
-  const colorWithValues = lines.slice(1).map((line) => line.split(' ').map(Number));
+  const count = BigInt(lines[0]);
+  const colorWithValues = lines.slice(1).map((line) => line.split(' ').map((n) => BigInt(n)));
 
   return calcColorsBoxCounts(count, colorWithValues);
 }
