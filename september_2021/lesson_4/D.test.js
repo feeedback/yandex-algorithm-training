1e93e03556a084aaed3bd8b38a06ba16bcd2a2b4
A. Rzhanitsyn <risdeep@gmail.com>
2021-09-13 14:03:13 +0500

    fix september_2021\lesson_4\D рефактор комментариев, добавил тест и его поддержку в коде "Если же для двух партий эти дробные части равны, то преимущество отдается той партии, которая получила большее число голосов"

--- september_2021/lesson_4/D.test.js
+++ september_2021/lesson_4/D.test.js
@@ -19,3 +19,23 @@ test('2', () => {
 test('3', () => {
   expect(fn(['Party number one 449', 'Partytwo 1'])).toStrictEqual(['Party number one 449', 'Partytwo 1']);
 });
+
+test('My "Если же для двух партий эти дробные части равны, то преимущество отдается той партии, которая получила большее число голосов"', () => {
+  expect(fn(['a 12', 'b 5', 'c 43'])).toStrictEqual(['a 90', 'b 37', 'c 323']);
+});
+
+// Поиск случая когда важно условия из теста выше
+
+// import { randomInt } from 'crypto';
+// const aCode = 'a'.charCodeAt();
+// const size = 3;
+// for (let n = 0; n < 10000; n++) {
+//   const data = new Array(size)
+//     .fill(null)
+//     .map((_, i) => `${String.fromCharCode(aCode + (i % size))} ${randomInt(1, 50)}`);
+//   const result = fn(data);
+//   if (result === null) {
+//     console.log(data);
+//     break;
+//   }
+// }
