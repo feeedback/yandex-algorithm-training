8faa6d84bd67618b102f6d6743587f6798433677
A. Rzhanitsyn <risdeep@gmail.com>
2021-11-02 23:35:33 +0500

    add september_2021_B\lesson_6\C temp

---
+++ september_2021_B/lesson_6/C.js
@@ -0,0 +1,184 @@
+// import { divisors0 } from './factr.js';
+import { setTimeout as delay } from 'timers/promises';
+
+const intDivision = (a, b) => Math.floor(a / b);
+
+const binarySearch = (leftInit = 0, rightInit, checkFn) => {
+  let left = leftInit;
+  let right = rightInit;
+
+  while (left < right) {
+    const middle = intDivision(left + right, 2);
+
+    if (checkFn(middle)) {
+      right = middle;
+    } else {
+      left = middle + 1;
+    }
+  }
+
+  return left;
+};
+
+/**
+ * "C. Корень кубического уравнения" {@link "https://contest.yandex.ru/contest/29188/problems/C/}
+ *
+ * Дано кубическое уравнение ax3+bx2+cx+d=0 (a≠0). Известно, что у этого уравнения есть ровно один
+ * корень. Требуется его найти.
+ *
+ * @param { number } a целые числа от -1000 до 1000
+ * @param { number } b целые числа от -1000 до 1000
+ * @param { number } c целые числа от -1000 до 1000
+ * @param { number } d целые числа от -1000 до 1000
+ *
+ * @return { x } Выведите единственный корень уравнения с точностью не менее 5 знаков после десятичной точки.
+ */
+function howManyElementsBetweenLR(sortedNums, N, num) {
+  const rIdx = binarySearch(0, N, (middleIndex) => sortedNums[middleIndex] > num);
+  const lIdx = binarySearch(0, N, (middleIndex) => sortedNums[middleIndex] >= num);
+
+  if (rIdx === lIdx) {
+    return [0, 0];
+  }
+  return [lIdx + 1, rIdx + 1 - 1];
+}
+
+const getDivisorsBruteForce = (num) => {
+  const arr = [];
+
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      const divisor = Math.floor(num / i);
+
+      if (divisor !== i) {
+        arr.push(divisor);
+      }
+      arr.push(i);
+    }
+  }
+  return arr.sort((a, b) => a - b);
+};
+const factorization = (numberInit) => {
+  const simpleDivisors = [];
+  let number = numberInit;
+  let i = 2;
+
+  while (true) {
+    const sqrtNumber = Math.sqrt(number);
+    i = 2;
+    if (number % i) {
+      i = 3;
+      while (number % i && i < sqrtNumber) {
+        i += 2;
+      }
+    }
+
+    if (i <= sqrtNumber) {
+      simpleDivisors.push(i);
+    } else if (i > sqrtNumber) {
+      simpleDivisors.push(number);
+      break;
+    }
+
+    number /= i;
+  }
+
+  return simpleDivisors;
+};
+const combinations = (array) =>
+  new Array(1 << array.length)
+    .fill()
+    .map((e1, i) => array.filter((e2, j) => i & (1 << j)))
+    .filter((a) => a.length >= 2)
+    .map((e) => e.reduce((multi, n) => multi * n, 1));
+
+const getDivisors = (number) => {
+  if (number < 10 ** 5) {
+    return getDivisorsBruteForce(number);
+  }
+
+  const factors = factorization(number);
+
+  if (factors.length > 9) {
+    return getDivisorsBruteForce(number);
+  }
+
+  const multipliersList = [1, ...new Set(combinations([1, ...factors]))];
+
+  return multipliersList.sort((a, b) => a - b);
+};
+const solveX = (a, b, c, d, x) => a * x ** 3 + b * x ** 2 + c * x + d;
+
+async function inputProcessing(lines) {
+  const coefficients = lines[0].split(' ').map(Number);
+
+  const [a, b, c, d] = coefficients;
+
+  const [divisorD, divisorA] = [getDivisors(Math.abs(d)), getDivisors(Math.abs(a))];
+  console.log({ divisorD, divisorA });
+  let betterDiff = Infinity;
+  let betterX = null;
+
+  for (let i = 0; i < divisorD.length; i += 1) {
+    for (let j = 0; j < divisorA.length; j += 1) {
+      let x = divisorD[i] / divisorA[j];
+      console.log(solveX(a, b, c, d, x));
+      if (solveX(a, b, c, d, x) === 0) {
+        return x;
+      }
+
+      if (Math.abs(solveX(a, b, c, d, x)) < betterDiff) {
+        betterDiff = Math.abs(solveX(a, b, c, d, x));
+        betterX = x;
+      }
+
+      x *= -1;
+      // x = -0.096991;
+      if (solveX(a, b, c, d, x) === 0) {
+        return x;
+      }
+
+      if (Math.abs(solveX(a, b, c, d, x)) < betterDiff) {
+        betterDiff = Math.abs(solveX(a, b, c, d, x));
+        betterX = x;
+      }
+      // console.log(x, solveX(a, b, c, d, x));
+    }
+  }
+  betterX = 1001;
+  betterDiff = 1001000;
+  console.log({ betterDiff, betterX });
+  let n = 1;
+
+  while (betterDiff > 0.001) {
+    let x = betterX - 0.001 * n;
+    console.log(x, { betterDiff, betterX });
+    if (betterDiff < 0.00001) {
+      return x;
+    }
+
+    if (Math.abs(solveX(a, b, c, d, x)) < betterDiff) {
+      betterDiff = Math.abs(solveX(a, b, c, d, x));
+      betterX = x;
+      n = 1;
+    }
+
+    x = betterX + 0.001 * n;
+    // console.log(x, { betterDiff, betterX });
+    if (betterDiff < 0.00001) {
+      return x;
+    }
+
+    if (Math.abs(solveX(a, b, c, d, x)) < betterDiff) {
+      betterDiff = Math.abs(solveX(a, b, c, d, x));
+      betterX = x;
+      n = 1;
+    }
+
+    await delay(15);
+    n += 1;
+  }
+  return betterX;
+}
+console.log(inputProcessing(['1 -1000 -1000 -1000']));
+export default inputProcessing;
---
+++ september_2021_B/lesson_6/factr.js
@@ -0,0 +1,227 @@
+import { randomInt } from 'crypto';
+
+const factorization = (number, accFactors = []) => {
+  const sqrtNumber = Math.sqrt(number);
+  let i = 2;
+
+  if (number % i) {
+    i = 3;
+    while (number % i && i < sqrtNumber) {
+      i += 2;
+    }
+  }
+  i = i <= sqrtNumber ? i : number;
+  if (i === number) {
+    return [...accFactors, i];
+  }
+  return factorization(number / i, [...accFactors, i]);
+};
+
+const printDivisors1 = (num) => {
+  const arr = [];
+
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      const divisor = Math.floor(num / i);
+
+      if (divisor !== i) {
+        arr.push(divisor);
+      }
+      arr.push(i);
+    }
+  }
+  return arr.sort((a, b) => a - b);
+};
+const factorization3 = (numberInit) => {
+  const simpleDivisors = [];
+  let number = numberInit;
+  let i = 2;
+
+  while (true) {
+    const sqrtNumber = Math.sqrt(number);
+    i = 2;
+    if (number % i) {
+      i = 3;
+      while (number % i && i < sqrtNumber) {
+        i += 2;
+      }
+    }
+
+    if (i <= sqrtNumber) {
+      simpleDivisors.push(i);
+    } else if (i > sqrtNumber) {
+      simpleDivisors.push(number);
+      break;
+    }
+
+    number /= i;
+  }
+
+  return simpleDivisors;
+};
+const combinations = (array) =>
+  new Array(1 << array.length)
+    .fill()
+    .map((e1, i) => array.filter((e2, j) => i & (1 << j)))
+    .filter((a) => a.length >= 2)
+    .map((e) => e.reduce((multi, n) => multi * n, 1));
+
+const divisors0 = (number) => {
+  if (number < 10 ** 5) {
+    return printDivisors1(number);
+  }
+
+  const factors = factorization3(number);
+
+  if (factors.length > 9) {
+    return printDivisors1(number);
+  }
+
+  const multipliersList = [1, ...new Set(combinations([1, ...factors]))];
+
+  return multipliersList.sort((a, b) => a - b);
+};
+// function printDivisors2(n) {
+//   const arr = [];
+
+//   for (let i = 1; i * i < n; i++) {
+//     if (n % i === 0) arr.push(i);
+//   }
+
+//   for (let j = Math.sqrt(n); j >= 1; j--) {
+//     if (n % j === 0) arr.push(n / j);
+//   }
+//   return arr;
+// }
+const factorization2 = (numberInit) => {
+  const simpleDivisors = [];
+  let number = numberInit;
+  let i = 2;
+  let sqrtNumber = Math.sqrt(number);
+
+  while (i <= sqrtNumber) {
+    sqrtNumber = Math.sqrt(number);
+    i = 2;
+    if (number % i) {
+      i = 3;
+      while (number % i && i < sqrtNumber) {
+        i += 2;
+      }
+    }
+
+    if (i <= sqrtNumber) {
+      simpleDivisors.push(i);
+    }
+    number /= i;
+  }
+  simpleDivisors.push(number);
+  return simpleDivisors;
+};
+
+const factorization0 = (number, accFactors = []) => {
+  const sqrtNumber = Math.sqrt(number);
+  let i = 2;
+  if (number % i) {
+    i = 3;
+    while (number % i && i < sqrtNumber) {
+      i += 2;
+    }
+  }
+  i = i <= sqrtNumber ? i : number;
+  if (i === number) {
+    return [...accFactors, i];
+  }
+  return factorization(number / i, [...accFactors, i]);
+};
+
+const divisors1 = (number) => {
+  const factors = factorization3(number).sort((a, b) => a - b);
+  if (factors.length === 1) {
+    return [1, factors[0]];
+  }
+
+  const multipliersList = [];
+  let product;
+
+  for (let i = 0; i < factors.length; i += 1) {
+    product = factors[i];
+    for (let j = i + 1; j < factors.length; j += 1) {
+      multipliersList.push(factors[i] * factors[j]);
+      product *= factors[j];
+      multipliersList.push(product);
+    }
+  }
+  return [1, ...Array.from(multipliersList), number];
+};
+const divisors2 = (number) => {
+  const factors = factorization3(number);
+  if (factors.length === 1) {
+    return [1, factors[0]];
+  }
+
+  const multipliersList = new Set(factors);
+  let product;
+
+  for (let i = 0; i < factors.length; i += 1) {
+    product = factors[i];
+    for (let j = 1; j < factors.length - 1; j += 1) {
+      multipliersList.add(factors[i] * factors[j]);
+      product *= factors[j];
+      multipliersList.add(product);
+    }
+  }
+  return [1, ...Array.from(multipliersList).sort((a, b) => a - b)];
+};
+// console.time('factorization');
+// divisors(1040224220);
+// console.timeEnd('factorization');
+
+// console.time('printDivisors 1');
+// printDivisors1(1040224220);
+// console.timeEnd('printDivisors 1');
+// console.time('factorization 1');
+// console.log(factorization(22244442));
+// console.timeEnd('factorization 1');
+// let sum1 = 0;
+// let sum2 = 0;
+
+// for (let n = 1; n < 20; n++) {
+//   // const num = randomInt(1, 2 ** n); // 281474976710654
+//   const num = randomInt(1, 281474976710654);
+//   // const num = randomInt(1, 10 ** 8);
+
+//   const start1 = Date.now();
+//   divisors0(num);
+//   const time1 = Date.now() - start1;
+//   sum1 += time1;
+
+//   const start2 = Date.now();
+//   printDivisors1(num);
+//   const time2 = Date.now() - start2;
+//   sum2 += time2;
+// }
+// console.log({ sum1, sum2 });
+
+export { divisors2, divisors0, printDivisors1 };
+// TODO https://shkolkovo.net/catalog/reshenie_uravnenij_2/kubicheskie
+
+// import { randomInt } from 'crypto';
+// import { divisors0, printDivisors1 } from './C.js';
+
+// for (let n = 1; n < 20; n++) {
+//   // const num = randomInt(1, 2 ** n); // 281474976710654
+//   const num = randomInt(1, 281474976710654);
+//   test(`${num}`, () => {
+//     const start1 = Date.now();
+//     const output1 = divisors0(num);
+//     const time1 = Date.now() - start1;
+
+//     const start2 = Date.now();
+//     const output2 = printDivisors1(num);
+//     const time2 = Date.now() - start2;
+
+//     expect(output1).toStrictEqual(output2);
+//     // expect(time1).toBeLessThanOrEqual(time2);
+//     console.log(time1, time2);
+//   });
+// }
