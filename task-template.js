957085bca3a1340735b01478cfdca76b65b272e1
Aleksandr Rzhanitsyn <risdeep@gmail.com>
2023-02-27 23:43:33 +0400

    add task-template

---
+++ task-template.js
@@ -0,0 +1,19 @@
+const fnLogic = () => {};
+
+/**
+ * "< task name >" {@link "< contest link >}
+ *
+ * < description >
+ *
+ * @param { string[] } lines < input description >
+ *
+ * @return { number }  < output description & type >
+ */
+function inputProcessing(lines) {
+  // const N = Number(lines[0]);
+  // const secondLineNumbers = lines[1].split(' ').map(Number);
+
+  return fnLogic();
+}
+
+export default inputProcessing;
